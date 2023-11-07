48121f6b018d1a1c979ee02153e540a7e2d7efe4
Dorian-Guignard <web.guidor@gmail.com>
2023-11-07 21:19:23 +0100

    controllerPatient

--- src/Controller/Api/PatientController.php
+++ src/Controller/Api/PatientController.php
@@ -2,20 +2,54 @@
 
 namespace App\Controller\Api;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/api/patients", name="api_patient_")
+ */
 class PatientController extends AbstractController
 {
     /**
-     * @Route("/api/patient", name="app_api_patient")
+     * @Route("/", name="list", methods={"GET"})
      */
-    public function index(): JsonResponse
+    public function list(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/Api/PatientController.php',
-        ]);
+        // Code pour récupérer la liste des patients depuis la base de données
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     */
+    public function show(int $id): JsonResponse
+    {
+        // Code pour afficher un patient spécifique par son ID
+    }
+
+    /**
+     * @Route("/", name="create", methods={"POST"})
+     */
+    public function create(Request $request): JsonResponse
+    {
+        // Code pour créer un nouveau patient en fonction des données de la requête
+    }
+
+    /**
+     * @Route("/{id}", name="update", methods={"PUT"})
+     */
+    public function update(int $id, Request $request): JsonResponse
+    {
+        // Code pour mettre à jour un patient existant en fonction des données de la requête
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete(int $id): JsonResponse
+    {
+        // Code pour supprimer un patient par son ID
     }
 }
+
