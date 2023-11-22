ee8edd37a5e91bcb241e4f8392391c097eff8e49
Dorian-Guignard <113593506+Dorian-Guignard@users.noreply.github.com>
2023-11-22 13:40:19 +0100

    AjoutEditDelete-PatientController

--- src/Controller/Api/PatientController.php
+++ src/Controller/Api/PatientController.php
@@ -119,20 +119,94 @@ public function creerConstante(Request $request, $id, EntityManagerInterface $en
 
 
 
-    /**
-     * @Route("/{id}", name="update", methods={"PUT"})
-     */
-    public function update(int $id, Request $request)
-    {
-        // Code pour mettre à jour un patient existant en fonction des données de la requête
+/**
+ * @Route("/patient/constantes/{id}", name="patient_constante_modifier", methods={"PUT"})
+ */
+public function modifierConstante(Request $request, EntityManagerInterface $entityManager, $id): Response
+{
+    $jsonContent = $request->getContent();
+    $requestData = json_decode($jsonContent, true);
+
+    // Vérifiez si les données nécessaires sont présentes et ont les types attendus
+    if (
+        !isset($requestData['value'], $requestData['date'], $requestData['time'], $requestData['constantType']) ||
+        !is_numeric($requestData['value']) ||
+        !DateTime::createFromFormat('Y-m-d', $requestData['date']) ||
+        !DateTime::createFromFormat('H:i:s', $requestData['time'])
+    ) {
+        return $this->json(['error' => 'Données invalides ou manquantes'], Response::HTTP_BAD_REQUEST);
     }
 
-    /**
-     * @Route("/{id}", name="delete", methods={"DELETE"})
-     */
-    public function delete(int $id)
-    {
-        // Code pour supprimer un patient par son ID
+    $patient = $this->getUser();
+    $constant = $entityManager->getRepository(Constant::class)->findOneBy(['id' => $id, 'patient' => $patient]);
+
+    if (!$constant) {
+        return $this->json(['error' => 'Constante non trouvée'], Response::HTTP_NOT_FOUND);
     }
+
+    try {
+        $entityManager->beginTransaction();
+
+        $constant->setValue($requestData['value']);
+        $constant->setDate(new \DateTime($requestData['date']));
+        $constant->setTime(new \DateTime($requestData['time']));
+
+        // Vérifiez si le type de constante existe dans la base de données
+        $constantTypeId = $requestData['constantType'];
+        $constantType = $entityManager->getRepository(ConstantType::class)->find($constantTypeId);
+
+        if (!$constantType) {
+            return $this->json(['error' => 'Type de constante invalide'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $constant->setConstantType($constantType);
+
+        // Enregistrez les modifications dans la base de données
+        $entityManager->flush();
+
+        $entityManager->commit();
+
+        return $this->json(['message' => 'Constante modifiée avec succès'], Response::HTTP_OK);
+    } catch (\Exception $e) {
+        // En cas d'échec, annulez la transaction
+        $entityManager->rollback();
+
+        // Loggez l'exception ou renvoyez une réponse d'erreur appropriée
+        return $this->json(['error' => 'Une erreur est survenue'], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
+
+/**
+ * @Route("/patient/constantes/{id}", name="patient_constante_supprimer", methods={"DELETE"})
+ */
+public function supprimerConstante(EntityManagerInterface $entityManager, $id): Response
+{
+    $patient = $this->getUser();
+    $constant = $entityManager->getRepository(Constant::class)->findOneBy(['id' => $id, 'patient' => $patient]);
+
+    if (!$constant) {
+        return $this->json(['error' => 'Constante non trouvée'], Response::HTTP_NOT_FOUND);
+    }
+
+    try {
+        $entityManager->beginTransaction();
+
+        // Supprimez la constante de la base de données
+        $entityManager->remove($constant);
+        $entityManager->flush();
+
+        $entityManager->commit();
+
+        return $this->json(['message' => 'Constante supprimée avec succès'], Response::HTTP_OK);
+    } catch (\Exception $e) {
+        // En cas d'échec, annulez la transaction
+        $entityManager->rollback();
+
+        // Loggez l'exception ou renvoyez une réponse d'erreur appropriée
+        return $this->json(['error' => 'Une erreur est survenue'], Response::HTTP_INTERNAL_SERVER_ERROR);
+    }
+}
+
+    
 }
 
